e85a9ecc618593c379212c21c6a77ae91280a1d7
nr-johnson <main.nrjohnson@gmail.com>
2022-08-12 22:30:51 +0000

    Got basic functionality

--- public/script.js
+++ public/script.js
@@ -1,16 +1,65 @@
-const socket = io('http://localhost:8000', {
+const socket = io('https://staging.nrjohnson.net', {
     withCredentials: true
 })
 
-const sendMessage = (event, form) => {
+const btns = document.querySelectorAll('.alert-btn')
+btns.length > 0 && btns.forEach(btn => {
+    btn.addEventListener('click', event => {
+        const level = parseInt(btn.getAttribute('data-level'))
+        socket.emit('message', {level: level, id: socket.id})
+    })
+})
+
+function sendMessage(event, form) {
     event.preventDefault()
     socket.emit('message', 'alert')
+    document.getElementById('homeMessage').innerHTML = 'Sending...'
 }
 
 socket.on('chat-message', data => {
     console.log(data)
 })
 
+let soundTimeout
+
 socket.on('alert', data => {
-    document.getElementById('message').innerHTML = data
+    const message = document.getElementById('message')
+    clearTimeout(soundTimeout)
+    if(message) {
+        const sound = document.getElementById('wifeAlert')
+        message.innerHTML = data.message 
+        socket.emit('received', {level: data.message, to: data.from})
+        sound.play()
+        soundTimeout = window.setTimeout(() => {
+            sound.pause()
+            sound.currentTime = 0
+        }, 20000)
+    }
 })
+
+let timeout
+
+socket.on('response', data => {
+    timeout && clearInterval(timeout)
+    const message = document.getElementById('homeMessage')
+    message.innerHTML = data.message
+    timeout = window.setTimeout(() => {
+        document.getElementById('homeMessage').innerHTML = ''
+    }, 5000)
+})
+
+function toggleCustomMessage(show) {
+    const form = document.getElementById('customForm')
+    if(show) {
+        form.classList.add('show')
+    } else {
+        form.classList.remove('show')
+        document.getElementById('customText').value = ''
+    }
+}
+
+function sendCustomMessage(event, input) {
+    event.preventDefault()
+    socket.emit('message', {message: input.value, level: 0, from: socket.id})
+    toggleCustomMessage(false)
+}
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const io = require('socket.io')(server, {
   cors: { origin: "*" }
 })
 
+let users= []
+
 app.set('view engine', 'pug')
 
 app.use(express.static(path.join(__dirname, '/public')))
@@ -19,12 +21,33 @@ app.get('/cave', (req, res) => {
 })
 
 io.on('connection', socket => {
-  console.log('New log in')
+  users.push(socket.id)
+  console.log(`User with id: ${socket.id}`)
+
   socket.on("message", data => {
-    socket.broadcast.emit('alert', 'Warning! The wife wants you!')
+    switch (data.level) {
+      case 1:
+        socket.broadcast.emit('alert', {level: 1, message: 'Come when you can', from: data.id})
+        break
+      case 2:
+        socket.broadcast.emit('alert', {level: 2, message: "I can't continue without you.", from: data.id})
+        break
+      case 3:
+        socket.broadcast.emit('alert', {level: 3, message: 'I need you ASAP.', from: data.id})
+        break
+      case 4:
+        socket.broadcast.emit('alert', {level: 4, message: 'Someone is about to die!', from: data.id})
+        break
+      default:
+        socket.broadcast.emit('alert', data)
+    }
+  })
+
+  socket.on('received', data => {
+    socket.broadcast.to(data.to).emit('response', {message: `Level ${data.level} alert delivered!`})
   })
 })
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8500
 
 server.listen(PORT, () => { console.log(`Server running on port ${PORT}`) })
